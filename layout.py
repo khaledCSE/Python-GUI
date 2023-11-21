02523312c08a69dc8c096d5e75cb999b82fd4635
khaledCSE <khaledcse10@gmail.com>
2023-11-21 20:55:45 +0600

    left part done

--- layout.py
+++ layout.py
@@ -1,5 +1,6 @@
 # import tkinter as tk
 # from tkinter import ttk
+from textwrap import fill
 import customtkinter as ctk
 
 class App(ctk.CTk):
@@ -53,11 +54,85 @@ class App(ctk.CTk):
       self.frame.rowconfigure(0, weight = 1, uniform = 'a')
       self.frame.pack(expand = True, fill = 'both')
 
-      ctk.CTkLabel(self.frame, text = 'Label 1', fg_color =  'red').grid(column = 0, row = 0, sticky = 'nsew', padx = 10, pady = 10)
-      ctk.CTkLabel(self.frame, text = 'Label 2', fg_color =  'green').grid(column = 1, row = 0, columnspan = 2, sticky = 'nsew', padx = 10, pady = 10)
-      ctk.CTkLabel(self.frame, text = 'Label 3', fg_color =  'blue').grid(column = 3, row = 0, sticky = 'nsew', padx = 10, pady = 10)
-      # ttk.Label(self.frame, text = 'Label 4', fg_color =  'yellow').grid(column = 3, row = 0, sticky = 'nsew', padx = 10, pady = 10)
+      # * Controls
+      left = ctk.CTkFrame(self.frame)
+      left.grid(column = 0, row = 0, sticky = 'nsew', padx = 10, pady = 10)
 
+      left_header = ctk.CTkLabel(left, text='Enter Data', font=('Roboto', 24), padx = 10, pady = 10)
+      image_chooser_label = ctk.CTkLabel(left, text='Choose an Image:', font=('Roboto', 14), padx = 10, pady = 20)
+      upload_button = ctk.CTkButton(left, text = 'Select Image', font=('Roboto', 14), border_spacing=10)
+      model_chooser_label = ctk.CTkLabel(left, text='Select a Model:', font=('Roboto', 14), padx = 10, pady = 20)
+      model_selector_combo = ctk.CTkComboBox(left, values=['YOLO v8', 'YOLO v7', 'YOLO v6', 'YOLO v5'], corner_radius=10)
+
+      # * Filter Selection
+      filter_selection_frame = ctk.CTkFrame(left, corner_radius=10)
+      image_filter_label = ctk.CTkLabel(filter_selection_frame, text='Image Filters', font=('Roboto', 18), pady=20)
+      resolution_label = ctk.CTkLabel(filter_selection_frame, text='Select Resolution', font=('Roboto', 14), pady=10)
+      resolution_selector_combo = ctk.CTkComboBox(filter_selection_frame, values=['Low', 'Medium', 'High'])
+      noise_label = ctk.CTkLabel(filter_selection_frame, text='Select Noise', font=('Roboto', 14), pady=10)
+      noise_selector_combo = ctk.CTkComboBox(filter_selection_frame, values=['Low', 'Medium', 'High'])
+      contrast_label = ctk.CTkLabel(filter_selection_frame, text='Select Contrast', font=('Roboto', 14), pady=10)
+      contrast_selector_combo = ctk.CTkComboBox(filter_selection_frame, values=['Low', 'Medium', 'High'])
+      color_label = ctk.CTkLabel(filter_selection_frame, text='Select Color', font=('Roboto', 14), pady=10)
+      color_selector_combo = ctk.CTkComboBox(filter_selection_frame, values=['Red', 'Green', 'Blue'])
+
+      filter_frame_empty_label = ctk.CTkLabel(filter_selection_frame, text='', pady=5)
+
+      image_filter_label.pack()
+      resolution_label.pack()
+      resolution_selector_combo.pack()
+      noise_label.pack()
+      noise_selector_combo.pack()
+      contrast_label.pack()
+      contrast_selector_combo.pack()
+      color_label.pack()
+      color_selector_combo.pack()
+      filter_frame_empty_label.pack()
+      
+      # * Packing
+      left_header.pack()
+      image_chooser_label.pack()
+      upload_button.pack()
+      model_chooser_label.pack()
+      model_selector_combo.pack()
+      filter_selection_frame.pack(expand = True, fill='x')
+
+      # * Call to action
+      proceed_button = ctk.CTkButton(left, text='Process Image', font=('Roboto', 18), border_spacing=10)
+      proceed_button.pack()
+
+      # * Empty Label for spacing
+      proceed_label = ctk.CTkLabel(left, text='', pady=5)
+      proceed_label.pack()
+
+      # ************* Left end ******************
+
+      # * Image
+      ctk.CTkLabel(self.frame, text = 'Label 2', fg_color =  'green').grid(column = 1, row = 0, columnspan = 2, sticky = 'nsew', padx = 10, pady = 10)
+
+      # * Info
+      right = ctk.CTkFrame(self.frame)
+      right.grid(column = 3, row = 0, sticky = 'nsew', padx = 10, pady = 10)
+
+      right_header = ctk.CTkLabel(right, text='Detected Parts', font=('Roboto', 24), padx = 10, pady = 10)
+
+      detection_result_frame = ctk.CTkFrame(right)
+
+      display_info_prompt = ctk.CTkLabel(right, text='Select a Part to Display', font=('Roboto', 14), padx = 10, pady = 10)
+      component_selector_combo = ctk.CTkComboBox(right, values=['Component 1', 'Component 2', 'Component 3'])
+      component_empty_label = ctk.CTkLabel(right, text='')
+      component_show_button = ctk.CTkButton(right, text='Show Component')
+
+      selected_result_frame = ctk.CTkFrame(right)
+
+      # * Packing
+      right_header.pack()
+      detection_result_frame.pack(expand=True, fill='x')
+      display_info_prompt.pack()
+      component_selector_combo.pack()
+      component_empty_label.pack()
+      component_show_button.pack()
+      selected_result_frame.pack(expand=True, fill='x')
 
 class SizeNotifier:
   def __init__(self, window, size_dict):
@@ -86,4 +161,5 @@ class SizeNotifier:
           self.current_min_size = checked_size
           self.size_dict[self.current_min_size]()
 
-app = App((800, 600))
+ctk.set_default_color_theme('green')
+app = App((1200, 720))
